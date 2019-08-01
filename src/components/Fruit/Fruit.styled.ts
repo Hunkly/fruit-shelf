946947ad25d3f77589b00c3aa2ba3c5b28496ad7
ViktorS <vityasapozhnik1998@gmail.com>
2019-08-01 15:18:28 +0300

    New component Fruit and three new images

---
+++ src/components/Fruit/Fruit.styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+interface IFruitProps {
+    size?: number;
+}
+
+export default styled.div<IFruitProps>`
+    
+    img{
+        width: ${props => (props.size)}00px;
+    }
+`
